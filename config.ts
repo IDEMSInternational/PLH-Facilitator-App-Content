d3419ba18fe2285d88a6e25239a85cbac6b0b418
EsmeeIDEMS <esmee@idems.international>
2023-05-25 17:13:46 +0100

    Add config

---
+++ config.ts
@@ -0,0 +1,26 @@
+import { generateDeploymentConfig } from "scripts";
+const config = generateDeploymentConfig("plh_facilitator");
+import { SKINS } from "./skins";
+
+/**
+ * The default config should ideally be a superset of any extended configs
+ * to allow for easier post-processing
+ */
+
+config.google_drive = {
+  sheets_folder_id: "1k8zJfBeOm18gYDsk0XmdUm7OFtZDUY9R",
+  assets_folder_id: "1KcHDI7O4o2_FZ_YlXsz-8OqN3ehsfdVf",
+};
+
+config.app_config.APP_LANGUAGES.default = "gb_en";
+config.app_config.APP_SIDEMENU_DEFAULTS.title = "FacilitatorApp";
+config.app_config.APP_HEADER_DEFAULTS.title = "FacilitatorApp";
+config.app_config.NOTIFICATION_DEFAULTS.title = "New message from FacilitatorApp";
+config.app_config.NOTIFICATION_DEFAULTS.text = "You have a new message from FacilitatorApp";
+config.app_config.APP_SKINS.defaultSkinName = SKINS.modular.name;
+config.app_config.APP_SKINS.available = [SKINS.modular, SKINS.weekly_workshop];
+config.app_config.APP_THEMES.available = ["default", "professional"];
+config.app_config.APP_UPDATES.enabled = true
+config.app_config.APP_UPDATES.completeUpdateTemplate = "app_update_complete"
+
+export default config;
